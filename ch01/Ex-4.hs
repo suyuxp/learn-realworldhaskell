6df70d1f9ef1007a8a82db4bb48e0b2856aa1caf
suyuxp <suyu@zhiyisoft.com>
2013-04-19 18:02:49 +0800

    修改函数命名

--- ch01/Ex-4.hs
+++ ch01/Ex-4.hs
@@ -1,5 +1,5 @@
 -- exercises for ch01
 -- 4. Modify the WC.hs example again, in order to print the number of characters in a file.
 
-main = interact wordCount
-    where wordCount input = show (length input) ++ "\n"
+main = interact charCount
+    where charCount input = show (length input) ++ "\n"
